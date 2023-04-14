9868ccda54a1e3b803ff9f42b673c2fb19e1acf6
amaan3212 <100236428+amaan3212@users.noreply.github.com>
2023-04-14 18:33:56 +0530

    Update demo1.js

--- demo1.js
+++ demo1.js
@@ -1,4 +1,6 @@
-function onClickMenu(){
-    document.getElementById("menu").classList.toggle("icon");
-    document.getElementById("nav").classList.toggle("change");
-}
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
